84e281e3bec126b79aea6be54d2d050ad6db230e
dimasriat <dimasriat@gmail.com>
2026-09-20 04:23:39 +0000

    web: show not-deployed card when pool is unavailable

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,20 +1,31 @@
 import { CapitalProvider } from "./components/CapitalProvider";
 import { FacilitiesList } from "./components/FacilitiesList";
 import { Header } from "./components/Header";
+import { NotDeployed } from "./components/NotDeployed";
 import { Originator } from "./components/Originator";
 import { PoolBoard } from "./components/PoolBoard";
+import { useDeployment } from "./hooks/useDeployment";
 
 export function App() {
+  const deployment = useDeployment();
+  const isDeployed = !!deployment?.pool;
+
   return (
     <div className="app">
       <Header />
       <main className="main">
-        <PoolBoard />
-        <div className="two-col">
-          <CapitalProvider />
-          <Originator />
-        </div>
-        <FacilitiesList />
+        {isDeployed ? (
+          <>
+            <PoolBoard />
+            <div className="two-col">
+              <CapitalProvider />
+              <Originator />
+            </div>
+            <FacilitiesList />
+          </>
+        ) : (
+          <NotDeployed />
+        )}
       </main>
     </div>
   );
